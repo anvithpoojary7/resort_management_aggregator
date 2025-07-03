2560c26e52288ef63ecca75f98795d5d7bb46567
anvithpoojary7 <anvithpoojary07@gmail.com>
2025-07-03 19:40:39 +0530

    added the requireauth.js

---
+++ server/middleware/requireAuth.js
@@ -0,0 +1,20 @@
+const jwt=require('jsonwebtoken');
+
+const requireAuth=(req,res,next)=>{
+    const token=req.cookies.token;
+    if(!token){
+        return res.status(401).json({message : 'unauthorized: no token provided'});
+
+    }
+    try{
+      const decoded=jwt.verify(token,process.env.JWT_SECRET);
+      req.user=decoded;
+      next();
+    }
+    catch(err){
+    return res.status(403).json({ message: 'Invalid token.' });
+    }
+}
+
+
+module.exports = requireAuth;
