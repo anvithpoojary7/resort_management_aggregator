e65edf5013ce79644d8b6fd5a16ed5d3d93c6f9c
Gowrika Alva <gowrikaalva@gmail.com>
2025-07-08 12:33:54 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,3 @@
-import React, { useState } from 'react';
-import Header from './Header';
-import SidePanel from './SidePanel';
-
-const Navbar = () => {
 import React, { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {
@@ -241,37 +236,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-  const user = isLoggedIn ? JSON.parse(localStorage.getItem('user')) : null;
-
-  const isOwner = user?.role === 'owner';
-
-  return (
-    <nav className="sticky top-0 w-full bg-white shadow p-4 z-50">
-      {/* Show Header only if not owner */}
-      {!isOwner && (
-        <Header
-          onMenuClick={() => setMenuOpen(true)}
-          isLoggedIn={isLoggedIn}
-          user={user}
-        />
-      )}
-
-      {/* Show SidePanel only if not owner */}
-      {!isOwner && (
-        <SidePanel
-          isOpen={menuOpen}
-          setIsOpen={setMenuOpen}
-          isLoggedIn={isLoggedIn}
-          user={user}
-        />
-      )}
-    </nav>
-  );
-};
-
-export default Navbar;
-
